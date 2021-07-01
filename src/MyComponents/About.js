ce81b9e91180d2d1d3ce7fd27fa43632ee6b5d41
RitikaKamboj <ritika.kamby@gmail.com>
2021-07-01 17:01:21 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ad ratione nulla ducimus maxime perferendis aut molestiae iure cumque eveniet amet culpa totam labore voluptatem consectetur vitae incidunt, itaque sit! Ab minima beatae eaque!</p>
+        </div>
+    )
+}
